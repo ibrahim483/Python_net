0080dbd97fc7e4d9fc6dacd8fa4b9763ea306680
ibrahim.baltah <ibrahim.abbalta@gmail.com>
2020-11-02 15:33:50 +0100

    tkinter project how to give colors to the text and the back ground and last not list how to place the and give them the size.

--- python_tkinter_1.py
+++ python_tkinter_1.py
@@ -2,19 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-topFrame = Frame(root)
-topFrame.pack()
-botomFrame = Frame(root)
-botomFrame.pack(side= BOTTOM)
+one = Label(root, text="one", bg="red", fg="white")
+one.pack()
+two = Label(root, text="two", bg="green", fg="black")
+two.pack(fill=X)
+three = Label(root, text="three", bg="blue", fg="white")
+three.pack(side=LEFT, fill=Y)
 
-button1 = Button(topFrame, text=" Button 1", fg="red")
-button2 = Button(topFrame, text=" Button 2", fg="blue")
-button3 = Button(topFrame, text=" Button 3", fg="green")
-button4 = Button(botomFrame, text=" Button 4", fg="yellow")
-
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack(side=BOTTOM)
 
 root.mainloop()
