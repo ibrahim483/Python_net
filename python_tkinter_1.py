d6821a4348f4be290fbcc3d790b8e476690d8d35
ibrahim.baltah <ibrahim.abbalta@gmail.com>
2020-11-02 15:10:35 +0100

    tkinter project how to do buttons and give tham a color an a place.

--- python_tkinter_1.py
+++ python_tkinter_1.py
@@ -1,6 +1,20 @@
 from tkinter import *
 
 root = Tk()
-theLabel = Label(root, text="this too easy ")
-theLabel.pack()
+
+topFrame = Frame(root)
+topFrame.pack()
+botomFrame = Frame(root)
+botomFrame.pack(side= BOTTOM)
+
+button1 = Button(topFrame, text=" Button 1", fg="red")
+button2 = Button(topFrame, text=" Button 2", fg="blue")
+button3 = Button(topFrame, text=" Button 3", fg="green")
+button4 = Button(botomFrame, text=" Button 4", fg="yellow")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=BOTTOM)
+
 root.mainloop()
